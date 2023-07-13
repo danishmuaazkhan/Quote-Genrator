09a5042579267748759712cdbe6824784070d164
Danish <devloperdanishkhan@gamil.com>
2023-07-14 03:31:30 +0530

    Add Twitter integration to the Twitter button 2- Change quote font size to 2rem for quotes longer than 120 characters 3- Display 'Unknown' as author name for quotes with missing author information

--- script.js
+++ script.js
@@ -1,18 +1,30 @@
 const apiUrl = 'https://type.fit/api/quotes';
 
 const newQuoteBtn = document.querySelector('.new-quote');
-const authorField = document.querySelector('.author');
-const quoteField = document.querySelector('.quote');
+const authorText = document.querySelector('.author');
+const quoteText = document.querySelector('.quote');
+const twitterBtn = document.querySelector('.twitter-button');
 
 let apiquotes = [];
 let quote = '';
 
 // Show New Quote 
-function newQuote () {
-    const randomIndex = Math.floor(Math.random()*apiquotes.length);
-     quoteField.innerHTML = apiquotes[randomIndex].text;
-     authorField.innerHTML = apiquotes[randomIndex].author;
-} 
+function newQuote() {
+
+    // Pick a random quote from apiquote array
+    const quote = apiquotes[Math.floor(Math.random() * apiquotes.length)];
+
+    // Check the code lenght to determine styling
+    if (quote.text.length > 120) quoteText.classList.add('long-quote');
+    else quoteText.classList.remove('long-quote');
+
+    quoteText.textContent = quote.text;
+
+    //Check if Author field is blank and replace it with 'Unknown'.
+    const setAuthor = !quote.author ? 'Unknown' : quote.author;
+    authorText.textContent = setAuthor;
+
+}
 
 async function getQuote() {
 
@@ -25,8 +37,19 @@ async function getQuote() {
     }
 }
 
- newQuoteBtn.addEventListener('click', () => {
-     newQuote();
-    })
-    getQuote();
-    
+// Tweet Quote
+function tweetQuote() {
+    const tweetUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} -${authorText.textContent}`;
+    window.open(tweetUrl, '_blanck');
+}
+
+// Event Listeners
+
+// New Quote Genreator
+newQuoteBtn.addEventListener('click', () => {
+    newQuote();
+})
+getQuote();
+
+// Tweet Quote
+twitterBtn.addEventListener('click', tweetQuote);
